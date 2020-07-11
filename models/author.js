35b7f5c7d1994e0c8663d3b7f371ac3d661f9d6e
maruxan <garavagliamauricio@gmail.com>
2020-07-11 20:58:17 -0300

    Base app skeleton and db connection

---
+++ models/author.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxlength: 100 },
+  family_name: { type: String, required: true, maxlength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtual for author's full name
+AuthorSchema.virtual('name').get(() => {
+  // Handling lack of names
+  let fullname = '';
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ', ' + this.first_name;
+  }
+  if (!this.first_name || !this.family_name) {
+    fullname = '';
+  }
+
+  return fullname;
+});
+
+// Virtual for author's lifespan
+AuthorSchema.virtual('lifespan').get(() => {
+  (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual('url').get(() => {
+  '/catalog/author/' + this._id;
+});
+
+// Export model
+module.exports = mongoose.model('Author', AuthorSchema);
---
+++ models/book.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const BookeSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'Author', required: true },
+  summary: { type: String, required: true },
+  isbn: { type: String, required: true },
+  genre: [{ type: Schema.Types.ObjectId, ref: 'Genre' }],
+});
+
+// Virtual for book's URL
+BookeSchema.virtual('url').get(() => {
+  '/catalog/book/' + this._id;
+});
+
+// Export model
+module.exports = mongoose.model('Book', BookeSchema);
