f7d3eda076f40173e95fb2ef3e11367ed7c2f9de
maruxan <garavagliamauricio@gmail.com>
2020-07-27 20:27:36 -0300

    author and genre delete feature

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -89,13 +89,70 @@ exports.genre_create_post = [
 ];
 
 // Display Genre delete form on GET.
-exports.genre_delete_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre delete GET');
+exports.genre_delete_get = function (req, res, next) {
+	async.parallel(
+		{
+			genre: function(callback) {
+				Genre.findById(req.params.id).exec(callback)
+			},
+
+			genres_books: function(callback) {
+				Book.find({ 'genre': req.params.id }).exec(callback);
+			}
+		},
+
+		function(err, results) {
+			if (err) return next(err);
+			if (results.genre == null) res.redirect('/catalog/genres');
+			// Successful, so render
+			res.render('genre_delete', 
+				{ 
+					title: 'Delete Genre',
+					genre: results.genre,
+					genre_books: results.genres_books 
+				}
+			);
+		}
+	);
 };
 
 // Handle Genre delete on POST.
-exports.genre_delete_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre delete POST');
+exports.genre_delete_post = function (req, res, next) {
+	async.parallel(
+		{
+			genre: function(callback) {
+				Genre.findById(req.body.genreid).exec(callback);
+			},
+
+			genres_books: function(callback) {
+				Book.find({ 'genre': req.body.genreid }).exec(callback);
+			}
+		},
+
+		function(err, results) {
+			if (err) return next(err);
+			// Success
+			if (results.genres_books.lenght > 0) {
+				// Genre has books, render in same way as for GET route
+				res.render('genre_delete', 
+					{
+						title: 'Delete Genre',
+						genre: results.genre,
+						genre_books: results.genres_books
+					}
+				)
+				return;
+			}
+			else {
+				// Genre has no books. Delete object and redirect to list of authors
+				Genre.findByIdAndRemove(req.body.genreid, function deleteGenre(err) {
+					if (err) return next(err);
+					// Success - go to genre list
+					res.redirect('/catalog/genres');
+				});
+			}
+		}
+	);
 };
 
 // Display Genre update form on GET.
